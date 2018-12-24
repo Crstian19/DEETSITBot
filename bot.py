170e2d265ab47d5bc6277abad51f10aa3089b95a
Crstian Gutierrez <cris_gl_3@hotmail.com>
2018-12-24 18:11:12 +0100

    Sendpdf dont work

--- bot.py
+++ bot.py
@@ -1,21 +1,23 @@
 import telebot
 from telebot import types
-import time
-
-TOKEN = '733447764:AAFPAT2C7I4MjbDz8bG34Twyzam3Uwyq2-k'
-
-AYUDA = '/help'
-
+TOKEN = 'TOKEN'
 bot = telebot.TeleBot(TOKEN)
-
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-	bot.reply_to(message, "Hola, Bienvenido al bot de la Delegación de Estudiantes de Teleco")
+    cid = message.chat.id
+    name = message.from_user.first_name
+    bot.send_message(cid, "Bienvenidx <b>" + name + "</b>!" ", este es el bot de la Delegación de Estudiantes de Teleco, para saber que puede hacer el bot usa el comando /help", parse_mode="HTML")
 
 @bot.message_handler(commands=['help'])
 def help_message(message):
-    msg = "Actualmente estos son los comandos disponibles para usar este bot /start /help"
+    msg = "Actualmente estos son los comandos disponibles para usar este bot /start /help /curso"
     bot.send_message(message.chat.id, msg)
+
+@bot.message_handler(commands=['documento'])
+def send_pdf(message):
+doc = open('/home/cristian/BOOOOT/users.txt', 'rb')
+bot.send_document(message.chat.id, doc)
+doc.close()
 
 bot.polling()

d94b885265273258ae8b720b4b9a09ac35d2872c
Crstian Gutierrez <cris_gl_3@hotmail.com>
2018-12-31 15:26:25 +0100

    Added Primer curso menu

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import telebot
 from telebot import types
-TOKEN = '733447764:AAHvEMWPaumZ-lXlkiwyhAb6ttYHvVcZpSs'
+TOKEN = 'BOT TOKEN'
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
@@ -11,27 +11,35 @@ def send_welcome(message):
 
 @bot.message_handler(commands=['help'])
 def help_message(message):
-    msg = "Actualmente estos son los comandos disponibles para usar este bot /start /help /curso"
+    msg = "Actualmente estos son los comandos disponibles para usar este bot /start /help /apuntes /examenes /calendario"
     bot.send_message(message.chat.id, msg)
 
 @bot.message_handler(commands=['calendario'])
-def send_pdf(message):
+def calendar(message):
     doc = open('/home/cristian/Downloads/Calendario.pdf', 'rb')
     bot.send_document(message.chat.id, doc)
     doc.close()
+
+@bot.message_handler(commands=['examenes'])
+def exams(message):
+    msg = "http://teleco.upct.es/wp-content/uploads/2018/12/CalendarioFebrero_AULAS.pdf"
+    bot.send_message(message.chat.id, msg)
+
 @bot.message_handler(commands=['apuntes'])
 def gen_markup(message):
-    markup = types.ReplyKeyboardMarkup(row_width=2)
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True,row_width=2)
     itembtn1 = types.KeyboardButton('Primero')
     itembtn2 = types.KeyboardButton('Segundo')
     itembtn3 = types.KeyboardButton('Tercero (GIT)')
     itembtn4 = types.KeyboardButton('Cuarto (GIT)')
-    markup.add(itembtn1, itembtn2, itembtn3, itembtn4)
+    itembtn5 = types.KeyboardButton('Tercero (GIST)')
+    itembtn6 = types.KeyboardButton('Cuarto (GIST)')
+    markup.add(itembtn1, itembtn2, itembtn3, itembtn4, itembtn5, itembtn6)
     bot.send_message(message.chat.id, "Elige un curso:", reply_markup=markup)
 
 @bot.message_handler(func=lambda message: message.text== 'Primero')
 def curso_primero(message):
-    subject_markup = types.ReplyKeyboardMarkup(row_width=2)
+    subject_markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
     itembtn1 = types.KeyboardButton('Cálculo 1')
     itembtn2 = types.KeyboardButton('Física')
     itembtn3 = types.KeyboardButton('Álgebra')
@@ -40,9 +48,23 @@ def curso_primero(message):
     bot.send_message(message.chat.id, "Elige una asignatura:", reply_markup=subject_markup)
 
 @bot.message_handler(func=lambda message: message.text== 'Cálculo 1')
-def send_pdf(message):
-    doc = open('/home/cristian/Downloads/Calendario.pdf', 'rb')
-    bot.send_document(message.chat.id, doc)
-    doc.close()
+def calculo1(message):
+    msg = "https://drive.google.com/open?id=0B7OtwXyjfmOZeFU4RUNZVmwtT2M"
+    bot.send_message(message.chat.id, msg)
+
+@bot.message_handler(func=lambda message: message.text== 'Álgebra')
+def algebra(message):
+    msg = "https://drive.google.com/file/d/0B7OtwXyjfmOZcUl3ZF9MMlV2NE0/view?usp=sharing"
+    bot.send_message(message.chat.id, msg)
+
+@bot.message_handler(func=lambda message: message.text== 'Física')
+def algebra(message):
+    msg = "Próximamente..."
+    bot.send_message(message.chat.id, msg)
+
+@bot.message_handler(func=lambda message: message.text== 'F.Programación')
+def algebra(message):
+    msg = "Próximamente..."
+    bot.send_message(message.chat.id, msg)
 
 bot.polling()

09f7fa800546ca07cffbf56da047066fc81102c8
Cristian <kiyoma19@gmail.com>
2019-03-18 14:33:24 +0100

    More subjects

--- bot.py
+++ bot.py
@@ -43,7 +43,14 @@ def curso_primero(message):
     itembtn2 = types.KeyboardButton('Física')
     itembtn3 = types.KeyboardButton('Álgebra')
     itembtn4 = types.KeyboardButton('F.Programación')
-    subject_markup.add(itembtn1, itembtn2, itembtn3, itembtn4)
+    itembtn5 = types.KeyboardButton('F.Computadores')
+    itembtn6 = types.KeyboardButton('Gestión de empresas')
+    itembtn7 = types.KeyboardButton('Cálculo 2')
+    itembtn8 = types.KeyboardButton('Sistemas y Circuitos')
+    itembtn9 = types.KeyboardButton('Estadística')
+    itembtn10 = types.KeyboardButton('F.Telemática')
+
+    subject_markup.add(itembtn1, itembtn2, itembtn3, itembtn4, itembtn5, itembtn6, itembtn7, itembtn8, itembtn9, itembtn10)
     bot.send_message(message.chat.id, "Elige una asignatura:", reply_markup=subject_markup)
 
 @bot.message_handler(func=lambda message: message.text== 'Cálculo 1')
@@ -57,13 +64,46 @@ def algebra(message):
     bot.send_message(message.chat.id, msg)
 
 @bot.message_handler(func=lambda message: message.text== 'Física')
-def algebra(message):
+def fisica(message):
     msg = "Próximamente..."
     bot.send_message(message.chat.id, msg)
 
 @bot.message_handler(func=lambda message: message.text== 'F.Programación')
-def algebra(message):
+def programacion(message):
     msg = "Próximamente..."
     bot.send_message(message.chat.id, msg)
+
+@bot.message_handler(func=lambda message: message.text== 'Física')
+def fisica(message):
+    msg = "Próximamente..."
+    bot.send_message(message.chat.id, msg)
+
+@bot.message_handler(func=lambda message: message.text== 'Física')
+def fisica(message):
+    msg = "Próximamente..."
+    bot.send_message(message.chat.id, msg)
+
+@bot.message_handler(func=lambda message: message.text== 'Física')
+def fisica(message):
+    msg = "Próximamente..."
+    bot.send_message(message.chat.id, msg)
+
+@bot.message_handler(func=lambda message: message.text== 'Física')
+def fisica(message):
+    msg = "Próximamente..."
+    bot.send_message(message.chat.id, msg)
+
+@bot.message_handler(func=lambda message: message.text== 'Segundo')
+def curso_segundo(message):
+    subject_markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    itembtn1 = types.KeyboardButton('Redes y Servicios')
+    itembtn2 = types.KeyboardButton('Conmutación')
+    itembtn3 = types.KeyboardButton('Ondas Electromagnéticas')
+    itembtn4 = types.KeyboardButton('Sistemas Lineales')
+    itembtn5 = types.KeyboardButton('Componentes y dispositivos electrónicos')
+
+
+    subject_markup.add(itembtn1, itembtn2, itembtn3, itembtn4, itembtn5, itembtn6, itembtn7, itembtn8, itembtn9)
+    bot.send_message(message.chat.id, "Elige una asignatura:", reply_markup=subject_markup)
 
 bot.polling()
